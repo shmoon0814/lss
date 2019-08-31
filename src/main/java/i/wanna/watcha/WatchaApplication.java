dcf22944b35e2b662d70ebfc98706753c7412473
msk2292 <msk2292@naver.com>
2019-08-31 15:19:13 +0900

    first init

---
+++ src/main/java/i/wanna/watcha/WatchaApplication.java
@@ -0,0 +1,14 @@
+package i.wanna.watcha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchaApplication.class, args);
+	}
+
+}
+
